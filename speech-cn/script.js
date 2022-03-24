a2718749024a9175e765a3efbe2bee8f806f4ad7
EvenyYlzz <172463074@qq.com>
2022-03-24 23:13:39 +0800

    feat: 引入babel/polyfill解决运行时报错，并调通页面，创建学习器

--- speech-cn/script.js
+++ speech-cn/script.js
@@ -1,8 +1,12 @@
-import * as speechCommands from '@tensorflow-models/speech-commands';
-import * as tfvis from '@tensorflow/tfjs-vis';
+import '@babel/polyfill'
+import * as speechCommands from '@tensorflow-models/speech-commands'
+// import * as tfvis from '@tensorflow/tfjs-vis'
 
 // 指向静态服务器的地址
-const MODEL_PATH = 'http://127.0.0.1:5500'
+const MODEL_PATH = 'http://127.0.0.1:8080'
+
+// 为了方便复用定一个迁移学习器变量
+let transferRecognizer
 
 window.onload = async () => {
   // 创建识别器
@@ -14,8 +18,23 @@ window.onload = async () => {
     // 参数3: 自定义模型的Url
     MODEL_PATH + '/speech/model.json',
     // 参数4: 源信息的Url
-    MODEL_PATH + './speech/metadata.json'
+    MODEL_PATH + '/speech/metadata.json'
   )
   // 确保加载完成
   await recognizer.ensureModelLoaded()
+  transferRecognizer = recognizer.createTransfer('轮播图') // 创建迁移学习器，接收一个参数，name进行命名
+}
+
+window.collect = async (btn) => {
+  btn.disabled = true
+  const label = btn.innerText
+  // _background_noise_ 包内参数
+  // 收集声音
+  await transferRecognizer.collectExample(
+    label === '背景噪音' ? '_background_noise_' : label
+  )
+  btn.disabled = false
+  document.querySelector('#count').innerHTML = JSON.stringify(transferRecognizer.countExamples(), null, 2)
+  // 查看数据收集情况
+  console.log(transferRecognizer.countExamples())
 }
